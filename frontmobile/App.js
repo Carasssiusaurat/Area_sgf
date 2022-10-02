21a4646aea0210acc7497eaf3a1fd6dd56595664
Saad Berrada <saadberrada@MacSaad.local>
2022-10-03 01:24:25 +0200

    first push login page

--- frontmobile/App.js
+++ frontmobile/App.js
@@ -6,112 +6,75 @@
  * @flow strict-local
  */
 
+import { View, Text, Button, Image, StyleSheet, TextInput} from "react-native";
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import BouncyCheckbox from "react-native-bouncy-checkbox";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+export default class Login extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      email : "",
+      password : "",
+    };
+    this.Inputs = this.Inputs.bind(this);
+  }
+  
+  Inputs = (props) => {
+    return(
+      <View>
+        <Text style = {{marginLeft : 40, color : "white"}}>{props.name}</Text>
+        <TextInput style={styles.input_style} onChangeText = {(text) =>
+        props.name = "Email" ?  this.setState({email : text}) : this.setState({password : text})}></TextInput>
+      </View>
+    );
+  }
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+  render(){
+    return(
+      <View style = {{backgroundColor : 'rgba(125, 19, 191, 1)', height : '100%'}}>
+          <Image style={[styles.center_image, {marginTop : 60}]} source={require('./assets/Logo.png')}></Image>
+          <this.Inputs name="Email"/>
+          <this.Inputs name="Password"/>
+          <View style = {{ flexDirection : "row"}}>
+          <BouncyCheckbox style = {{marginLeft : 30}} iconStyle = {styles.Check_box_style} fillColor = 'rgba(0, 0, 0, 0)' onPress={() => {}} />
+          <Text style = {{color : "white"}}>Remember me ?</Text>
+          </View>
+          {/* EN CHANTIER */}
+      </View>
+      
+      
+      
+      );
+  }
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
-export default App;
+  center_image : {
+    marginLeft : "auto", 
+    marginRight : "auto"
+  },
+  input_style : {
+    height: 50, 
+    margin: 12, 
+    borderWidth: 1, 
+    padding: 10, 
+    backgroundColor : 'rgba(180, 179, 179, 1)', 
+    borderRadius : 15, 
+    width : "80%",
+    marginLeft : "auto",
+    marginRight : "auto"
+  },
+  Check_box_style : {
+    backgroundColor : "black", 
+    borderRadius : 0, 
+    borderColor : "black", 
+    width : 20, 
+    height : 20
+  }
+
+
+
+});
