6141b81bdea7943cf1f4216a7da7b142902c31de
CherifAziz <mohamed-aziz.cherif@epitech.eu>
2022-11-08 19:35:48 +0100

    [DELETE] empty branch

--- BackEnd/routes/about_json.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const about_json = require('../controllers/about_jsonController');
-
-router.get('/about.json', about_json.getAboutJson);
-
-module.exports = router;
