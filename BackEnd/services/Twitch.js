90f2e88174308800e2c360e7790744c1a1cd4a49
Duncan Darmon <duncan.darmon@epitech.eu>
2022-11-11 15:38:52 +0100

    [UPDATE] fixed error handling axios and params check

--- BackEnd/services/Twitch.js
+++ BackEnd/services/Twitch.js
@@ -26,8 +26,7 @@ passport.use(
 );
 
 UserIdFromName = async (token, username) => {
-  console.log(token)
-  console.log(username)
+  try {
   const data = await axios.get('https://api.twitch.tv/helix/users?login=' + username, {
     headers: {
       Authorization: "Bearer " + token,
@@ -35,6 +34,10 @@ UserIdFromName = async (token, username) => {
     },
   },);
   return(data.data.data[0].id)
+  } catch(err) {
+    console.log(err)
+    res.send(err)
+  }
 };
 
 twitchWhisp = async (req, res) => {
@@ -42,8 +45,6 @@ twitchWhisp = async (req, res) => {
   self_id = req.body.user_id
   receiver = req.body.to
   receiver_id = await UserIdFromName(usertoken, receiver)
-  console.log("3", receiver_id)
-  url_dest = 'https://api.twitch.tv/helix/whispers?from_user_id=' + self_id + "&to_user_id=" + receiver_id
   let body = {"message": req.body.message }
   try {
   const data = await axios.post('https://api.twitch.tv/helix/whispers?from_user_id=' + self_id + "&to_user_id=" + receiver_id,  body, {
@@ -55,6 +56,7 @@ twitchWhisp = async (req, res) => {
   res.status(200).send("message sent succesfully")
   } catch(err) {
     console.log(err)
+    res.send(err)
   }
 }
 
@@ -63,9 +65,11 @@ router.post('/twitch/whisp', async function(req, res) {
 });
 
 GetUserId = async (req, res) => {
+  if (!req.user.accessToken) {
+    res.status(400).send("error: missing field")
+  }
   let token = req.user.accessToken;
-  console.log("Bearer " + token)
-  console.log("client_id", process.env.TWITCH_CLIENT_ID)
+  try {
   const data = await axios.get('https://api.twitch.tv/helix/users' , {
     headers: {
       'Authorization': "Bearer " + token,
@@ -73,13 +77,15 @@ GetUserId = async (req, res) => {
     },
   },);
   return(data.data.data[0].id)
+  } catch(err) {
+    console.log(err)
+    return(err)
+  }
 };
 
 GetUserIdFromName = async (req, res) => {
   let token = req.headers.authorization.split(' ')[1]
-  console.log("auth", req.headers.authorization)
-  console.log("username", req.body.username)
-  console.log(token)
+  try {
   const data = await axios.get('https://api.twitch.tv/helix/users?login='+ req.body.username, {
     headers: {
       Authorization: "Bearer " + token,
@@ -87,6 +93,10 @@ GetUserIdFromName = async (req, res) => {
     },
   },);
   return(data.data.data[0].id)
+  } catch(err) {
+    console.log(err)
+    return(err)
+  }
 };
 
 router.get('/get_user_id/name', function(req, res) {
@@ -100,8 +110,6 @@ router.get('/get_user_id/name', function(req, res) {
     });
   } else {
   GetUserId(req).then((data) => {
-    console.log("USERID", data)
-    console.log("TOKEN", req.user.acessToken)
     res.json({user_id: data, bearer_token:req.headers.authorization});
   }).catch((error) => {
     res.status(500).send("error: failed to get requested ID");
@@ -120,8 +128,6 @@ router.get('/get_user_id', function(req, res) {
     });
   } else {
   GetUserId(req).then((data) => {
-    console.log("USERID", data)
-    console.log("TOKEN", req.user.acessToken)
     res.json({user_id: data, bearer_token:req.user.acessToken});
   }).catch((error) => {
     res.status(500).send("error: failed to get requested ID");
@@ -136,46 +142,48 @@ router.get('/auth/twitch/callback',
   twitchToken = req.user.accessToken;
 
   GetUserId(req).then((data) => {
-    console.log("userid", data)
-    console.log("accessToken", req.user.accessToken)
     res.json({user_id: data, bearer_token:req.user.accessToken});
-  }).catch((error) => {
-    console.log(error)
-    console.log("accesstoken", req.user.accessToken) 
+  }).catch((error) => { 
     res.status(500).send("error: couldn't get user Id")
   });
 });
 
-router.get('/twitch/getFollows', async function (req, res) {
-  //ERROR CHECK PARAMS
+twitchGetFollows = async (req, res) => {
+  if (!req.headers.authorization || !req.query.user_id) {
+    res.send("error: missing fields")
+  }
   let usertoken = req.headers.authorization
   let user_id = req.query.user_id
+  try {
   const data = await axios.get("https://api.twitch.tv/helix/streams/followed?user_id="+ user_id, {
     headers: {
       Authorization: usertoken,
       'Client-ID': process.env.TWITCH_CLIENT_ID
   }});
-  //check data is valid:
-  //if not, then return error status
-  if (!data || !data.data || !data.data.data) {
-    res.status(500).send("error processing request: empty return")
-  }
-  console.log(data.data.data[0])
   res.json(data.data.data)
+  } catch(err) {
+    console.log(err)
+    res.send(err)
+  }
+}
+
+router.get('/twitch/getFollows', async function (req, res) {
+  twitchGetFollows(req, res)
 });
 
 anyIsStreaming = async (req, res) => {
-  //ERROR CHECK PARAMS
+  if (!req.headers.authorization || !req.query.user_id) {
+    res.status(400).send("error: missing field")
+  }
   let usertoken = req.headers.authorization
   let user_id = req.query.user_id
+  try {
   const data = await axios.get("https://api.twitch.tv/helix/streams/followed?user_id="+ user_id, {
     headers: {
       Authorization: usertoken,
       'Client-ID': process.env.TWITCH_CLIENT_ID
-  }});//CATCH ERR
-  if (!data || !data.data || !data.data.data) {
+  }});
     res.status(500).send("error processing request: empty return")
-  }
   let vals = data.data.data
   const streamers = vals.filter(e => e.type === "live")
   if(streamers.length > 0) {
@@ -184,6 +192,10 @@ anyIsStreaming = async (req, res) => {
     console.log("FALSE")//TEMP RETURN
   }
   res.json(data.data.data)//TEMP RETURN
+  } catch(err) {
+    console.log(err)
+    res.send(err)
+  }
 }
 
 router.get('/twitch/streaming/any', async function (req, res) {
@@ -191,21 +203,19 @@ router.get('/twitch/streaming/any', async function (req, res) {
 });
 
 streamerIsStreaming = async (req, res) => {
-  //err params check
   if (!req.headers.authorization || !req.query.user_id || !req.body.broadcaster) {
     res.json("error: one or several fields missing (headers.authorization: query.user_id: body.broadcaster)")
   }
   let usertoken = req.headers.authorization
   let user_id = req.query.user_id
   let username = req.body.broadcaster
+  try {
   const data = await axios.get("https://api.twitch.tv/helix/streams/followed?user_id="+ user_id, {
     headers: {
       Authorization: usertoken,
       'Client-ID': process.env.TWITCH_CLIENT_ID
-  }});//catch err
-  if (!data || !data.data || !data.data.data) {
+  }});
     res.status(500).send("error processing request: empty return")
-  }
   let vals = data.data.data
   const streamer = vals.find(e => e.user_name === username)
   if(streamer.type === "live") {
@@ -214,6 +224,10 @@ streamerIsStreaming = async (req, res) => {
     console.log("FALSE")//TEMP RETURN
   }
   res.json(data.data.data)//TEMP RETURN
+  } catch(err) {
+  console.log(err)
+  res.send(err)
+  }
 }
 
 router.get('/twitch/streaming/broadcaster', async function (req, res) {
