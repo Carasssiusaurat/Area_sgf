624b9014842908973d7a30b8f8907d342c201ae4
yoann <yoann.elasmar@epitech.eu>
2022-11-13 19:15:53 +0100

    [MOD] Gitlab oauth system

--- src/Gitlab.js
+++ src/Gitlab.js
@@ -3,6 +3,7 @@ var passport = require('passport');
 var GitLabStrategy = require('passport-gitlab2').Strategy;
 var session = require('express-session');
 const fetch = require("node-fetch");
+const { Router } = require('express');
 let gitlab;
 const app = express();
 
@@ -10,10 +11,6 @@ app.use(session({
   secret: 'SECRET', resave: true,
   saveUninitialized: true
 }));
-
-app.listen(8080, () => {
-  console.log('Serveur en écoute sur le port 8080');
-});
 
 passport.serializeUser(function (user, done) {
   done(null, user);
@@ -24,16 +21,39 @@ passport.deserializeUser(function (user, done) {
 });
 
 passport.use(new GitLabStrategy({
-  clientID: GITLAB_CLIENT_ID,
-  clientSecret: GITLAB_CLIENT_SECRET,
-  callbackURL: "http://localhost:8080/gitlab/auth/callback"
+  clientID: process.env.GITLAB_CLIENT_ID,
+  clientSecret: process.env.GITLAB_CLIENT_SECRET,
+  callbackURL: "http://localhost:8080/service/gitlab/auth/callback",
+  passReqToCallback: true
 },
   function (accessToken, refreshToken, profile, done) {
-    gitlab = new Gitlab(accessToken, "jaredhanson");
-    console.log(profile);
-    return done(null, profile);
+    return done(null, {accessToken, refreshToken, profile});
   }
 ));
+
+const gitlab_auth = async (req, res, next) => {
+  const GITLAB_SCOPES = ['api'];
+  passport.authenticate('gitlab', {scope: GITLAB_SCOPES, showDialog: true, state: "token=" + req.query.token + ",serviceid=" + req.query.service_id})(req, res, next);
+}
+
+router.get('/auth', gitlab_auth);
+
+router.get('/auth/callback',
+  passport.authenticate('gitlab', { failureRedirect: '/login'}),
+  async function(req, res) {
+    console.log(req.user);
+    const user_id = req.query.state.split(",")[0].split("=")[1];
+    const service_id = req.query.state.split(",")[1].split("=")[1];
+    console.log(req.user.accessToken)
+    response = await addservice_copy(user_id, service_id, req.user.accessToken, req.user.refreshToken, null);
+    console.log(response);
+    if (response.status != 200) {
+      console.log("error");
+      console.log(response.message)
+    }
+    console.log("GitLab service added");
+    res.redirect('http://localhost:8081/home');
+  });
 
 app.get('/auth/gitlab',
   passport.authenticate('gitlab', { scope: ['api'] }));
